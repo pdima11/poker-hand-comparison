21896533a7a454500bf62ea87043d9ba5b4ae709
pdima11 <dzmitry.padvalnikau@gmail.com>
2020-03-07 01:00:00 +0300

    Add Boot

---
+++ src/main/scala/Boot.scala
@@ -0,0 +1,16 @@
+import com.evolutiongaming.util.InputParser
+
+import scala.io.StdIn
+
+
+object Boot extends App {
+  val inputs  = Iterator.continually(StdIn.readLine)
+    .takeWhile(_.nonEmpty)
+    .toList
+
+  inputs.foreach { line =>
+    val hands = InputParser.parseLine(line)
+    println(hands)
+  }
+
+}
