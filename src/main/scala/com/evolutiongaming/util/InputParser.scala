890e0097ee1d78f3616006586675ad8532b0176f
pdima11 <dzmitry.padvalnikau@gmail.com>
2020-03-07 00:57:28 +0300

    Add parser that will transform input string to hand objects

---
+++ src/main/scala/com/evolutiongaming/util/InputParser.scala
@@ -0,0 +1,12 @@
+package com.evolutiongaming.util
+
+import com.evolutiongaming.model.{Card, Hand}
+
+object InputParser {
+  def parseLine(line: String): List[Hand] = {
+    val input = line.trim.split(" ").toList
+    val boardCards = Card.fromString(input.head)
+
+    input.tail.map(Card.fromString).map(handCards => Hand(handCards, handCards ::: boardCards))
+  }
+}
