4c5d7e7f0a649ba35462bd098f8ada811b75b7ee
pdima11 <dzmitry.padvalnikau@gmail.com>
2020-03-07 18:32:00 +0300

    Add possibility to run app with comand line argument

--- src/main/scala/Boot.scala
+++ src/main/scala/Boot.scala
@@ -1,21 +1,27 @@
-import com.evolutiongaming.comparator.OmahaComparator
-import com.evolutiongaming.parser.OmahaInputParser
+import com.evolutiongaming.comparator.{OmahaComparator, TexasHoldemComparator}
+import com.evolutiongaming.parser.{OmahaInputParser, TexasHoldemParser}
 import com.evolutiongaming.util.OutputWriter
 
 import scala.io.StdIn
+import scala.util.{Failure, Success, Try}
 
 
 object Boot extends App {
+  val pokerType: Try[String] = Try(args(0))
+
+  val (parser, comparator) = pokerType match {
+    case Success("--omaha")                => (OmahaInputParser, OmahaComparator)
+    case Success("--texas-holdem") | Failure(_)  => (TexasHoldemParser, TexasHoldemComparator)
+  }
+
   val inputs  = Iterator.continually(StdIn.readLine)
     .takeWhile(_.nonEmpty)
     .toList
 
-  inputs.foreach { line =>
-    val hands = OmahaInputParser.parseLine(line)
-
-    val combinations = OmahaComparator.compare(hands)
-
-    OutputWriter.write(combinations, print)
-  }
-
+  inputs
+    .map { line =>
+      val hands = OmahaInputParser.parseLine(line)
+      OmahaComparator.compare(hands)
+    }
+    .foreach(OutputWriter.write(_, print))
 }
